edc7f1991c334c8e4c3d6381e562cad21c2f3b4b
Sziad08 <adamrszigeti@gmail.com>
2022-08-30 18:40:08 -0600

    E

---
+++ src/main/java/net/sziad/toastedtestmod/ToastedTestMod.java
@@ -0,0 +1,15 @@
+package net.sziad.toastedtestmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ToastedTestMod implements ModInitializer {
+	public static final String MOD_ID = "toastedtestmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/sziad/toastedtestmod/ToastedTestModClient.java
@@ -0,0 +1,10 @@
+package net.sziad.toastedtestmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ToastedTestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
